683b7c2a9b84ac593a008a275361cad0d3257302
pedrojsn96 <pjsn@cin.ufpe.br>
2016-05-07 03:50:32 +0000

    Realiza teste de comportamento
    Altera ir_para_new_user_step.rb
    Altera ir_para_new_company_step.rb
    Resolve problema do teste de login company
    Usa tag visible: false

--- features/step_definitions/ir_para_new_company_step.rb
+++ features/step_definitions/ir_para_new_company_step.rb
@@ -4,10 +4,14 @@ end
 
 When(/^I press sign up company now link$/) do
     visit (sign_in_login_path)
-    #click_button('btn_company')
-    #click_link('link_company')
-    #find_by_id('div_aba2')
-    #click_link('link_company')
+    # Para tag que não estao visiveis pode utilizar:
+        #Capybara.ignore_hidden_elements = false
+        #Ou fazer usando visible: false
+    within('div#box_login') do
+        click_on('btn_company')
+        page.find('#div_aba2', visible: false).click_link('link_company', visible: false)
+    end
+   # Capybara.ignore_hidden_elements = true
 end
 
 Then(/^I should be on the Sign Up Company Page$/) do
--- features/step_definitions/ir_para_new_user_step.rb
+++ features/step_definitions/ir_para_new_user_step.rb
@@ -4,7 +4,8 @@ end
 
 When(/^I press sign up user now link$/) do
     visit (sign_in_login_path)
-    click_link('link_user')
+    page.find('#div_user').click_link('link_user')
+    #click_link('link_user')
 end
 
 Then(/^I should be on the Sign Up User Page$/) do
