9dcac3fa4220a74ad0d018070d7f618fc006f86f
pedrojsn96 <pjsn@cin.ufpe.br>
2016-06-08 15:08:42 +0000

    Add Review em Question #47
    Cria model e controller de Review
    Altera arquivo de rotas
    Add função em user_controller
    Associa review com question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
   belongs_to :exame
+  has_many :reviews
   validates :nome, presence: true, length: {maximum: 200}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517113245) do
+ActiveRecord::Schema.define(version: 20160608125551) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "nota"
+    t.text     "comment"
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id"
 
   create_table "companies", force: :cascade do |t|
     t.string   "cnpj"
@@ -38,6 +50,16 @@ ActiveRecord::Schema.define(version: 20160517113245) do
   end
 
   add_index "questions", ["exame_id"], name: "index_questions_on_exame_id"
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "nota"
+    t.text     "comentario"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "reviews", ["question_id"], name: "index_reviews_on_question_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "nome"
