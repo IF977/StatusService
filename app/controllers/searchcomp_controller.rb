04f677670c94cd26551539afaed7bf10d3142dc5
Carlos Melo <chnm@cin.ufpe.br>
2016-06-20 17:56:32 +0000

    Conserta imagens (talvez)

--- app/controllers/searchcomp_controller.rb
+++ app/controllers/searchcomp_controller.rb
@@ -1,7 +1,8 @@
 class SearchcompController < ApplicationController
     def searchcomp
         if params[:search]
-            @search = Company.where(nome: params[:search].downcase)
+            #@search = ActiveRecord::Company.connection.execute("SELECT * FROM Company WHERE (Company.nome IN '#{params[:search].downcase}')")
+            @search = Company.where(:nome => params[:search].downcase)
         else
             @search = Company.where(nome: params[:search])
         end
